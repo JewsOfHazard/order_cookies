9ff5b5f69e631b1ea4bcdc58cc4ba566bd53a08c
JewsOfHazard <logan.saso2016@gmail.com>
2016-11-07 21:50:32 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# OSU Order Cookies
+## Setup
+
+* Make a new text document in the same directory of order_cookies.py formatted as such:
+    * Line 1 - OSU Username (hazardj for example)
+    * Line 2 - OSU Password (123456789 for example)
+    * Line 3 - OSU Student ID (935999999 for example)
+    * Line 4 - Phone Number (Optional for the website, but needed for the script)
+
+## Usage
+
+`python3 order_cookies.py`
+
+OPTIONAL
+
+`python3 -m "Message for workers. (Please deliver to _____ for example)"`
---
+++ order_cookies.py
@@ -0,0 +1,41 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import argparse
+
+parser = argparse.ArgumentParser(prog="PROG")
+parser.add_argument("-m", "--message", nargs="?", default="", help="Extra message to add to details.")
+arguments = parser.parse_args()
+
+chromedriver = webdriver.Chrome()
+chromedriver.get("https://app.uhds.oregonstate.edu/food2you/")
+
+parameters = []
+with open("config.txt", "r+") as opened:
+    for line in opened:
+        parameters.append(line)
+
+element = chromedriver.find_element_by_id('username')
+element.send_keys(parameters[0])
+element = chromedriver.find_element_by_id('password')
+element.send_keys(parameters[1])
+
+# element = chromedriver.find_element_by_class("6FreshBakedCookies")
+# element.click()
+
+element = chromedriver.find_element_by_css_selector("[data-id='41']")
+element.click()
+
+element = chromedriver.find_element_by_id("verify")
+element.send_keys(parameters[2])
+
+element = chromedriver.find_element_by_id("phone")
+element.send_keys(parameters[3])
+
+if arguments.message != None:
+    element = chromedriver.find_element_by_id("notes")
+    element.send_keys(arguments.message)
+
+element = chromedriver.find_element_by_xpath('//*[@id="order"]/div/div[4]/input')
+element.click()
